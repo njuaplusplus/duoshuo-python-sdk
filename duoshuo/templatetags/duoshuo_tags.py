59b2c10312d767aeb26c0e206f8a447a1b501a84
Aplusplus <njuaplusplus@gmail.com>
2014-12-24 19:47:29 +0800

    Add the next url in sso login logout

--- duoshuo/templatetags/duoshuo_tags.py
+++ duoshuo/templatetags/duoshuo_tags.py
@@ -66,15 +66,19 @@ def my_duoshuo_comments(data_thread_key, data_title, data_url):
     return code
 
 @register.simple_tag
-def my_sso_duoshuo_comments(data_thread_key, data_title, data_url):
+def my_sso_duoshuo_comments(data_thread_key, data_title, data_url, next_url=None):
     ''' 生成和通用代码一致的代码
 
     data_thread_key 文章在站点中的ID
     data_title      文章的标题
     data_url        文章的网址
-    login           login url for sso
-    logout          logout url for sso
+    next_url        用于返回登录前页面
     '''
+    login_url = SSO_LOGIN_URL
+    logout_url = SSO_LOGOUT_URL
+    if next_url is not None and len(next_url) > 0:
+        login_url = "%s?next=%s" % (SSO_LOGIN_URL, next_url)
+        logout_url = "%s?next=%s" % (SSO_LOGOUT_URL, next_url)
     code = '''<!-- Duoshuo Comment BEGIN -->
     <div class="ds-thread" data-thread-key="%s" data-title="%s" data-url="%s"></div>
     <script type="text/javascript">
@@ -94,16 +98,20 @@ def my_sso_duoshuo_comments(data_thread_key, data_title, data_url):
          || document.getElementsByTagName('body')[0]).appendChild(ds);
     })();
     </script>
-    <!-- Duoshuo Comment END -->''' % (data_thread_key, data_title, data_url, DUOSHUO_SHORT_NAME, SSO_LOGIN_URL, SSO_LOGOUT_URL)
+    <!-- Duoshuo Comment END -->''' % (data_thread_key, data_title, data_url, DUOSHUO_SHORT_NAME, login_url, logout_url)
     return code
 
 @register.simple_tag
-def my_sso_duoshuo_login():
+def my_sso_duoshuo_login(next_url=None):
     ''' 生成和通用代码一致的代码
 
-    login           login url for sso
-    logout          logout url for sso
+    next_url        用于返回登录前页面
     '''
+    login_url = SSO_LOGIN_URL
+    logout_url = SSO_LOGOUT_URL
+    if next_url is not None and len(next_url) > 0:
+        login_url = "%s?next=%s" % (SSO_LOGIN_URL, next_url)
+        logout_url = "%s?next=%s" % (SSO_LOGOUT_URL, next_url)
     code = '''<!-- Duoshuo Comment BEGIN -->
     <div class="ds-login"></div>
     <script type="text/javascript">
@@ -123,7 +131,7 @@ def my_sso_duoshuo_login():
          || document.getElementsByTagName('body')[0]).appendChild(ds);
     })();
     </script>
-    <!-- Duoshuo Comment END -->''' % (DUOSHUO_SHORT_NAME, SSO_LOGIN_URL, SSO_LOGOUT_URL)
+    <!-- Duoshuo Comment END -->''' % (DUOSHUO_SHORT_NAME, login_url, logout_url)
     return code
 
 @register.filter
